70de8d23d36d0f70e3cd6a28b35025733287b1ee
Anmol Jhamb <codingwithaj@gmail.com>
2021-10-29 13:09:24 +0530

    removed the redundant code from increment function

--- Bitwise_Algos/add_1_without_using_plus.cpp
+++ Bitwise_Algos/add_1_without_using_plus.cpp
@@ -4,13 +4,6 @@ using namespace std;
 
 int increment(int &number)
 {
-    // find the right most unset bit, and flip that, and then, unset all the bits right of that
-    if( number & 1 == 0 )
-    {
-        // it's a even number, we can just return number | 1 
-        return number | 1;
-    }
-
     // 111
     int index = 0;
     int temp = number;
@@ -27,7 +20,7 @@ int increment(int &number)
 
 int main()
 {
-    int number = 13;
+    int number = 14;
     cout << increment(number);
     return 0;
 }
